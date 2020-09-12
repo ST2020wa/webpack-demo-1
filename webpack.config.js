7636626c53db24c880a0b2e47295bfb5c466a34d
ST <shaotian.li.april@gmail.com>
2020-09-12 17:51:19 +0800

    v3

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,23 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 var path = require('path');
 
 module.exports = {
     mode: 'development',
     entry: './src/index.js',
     output: {
-        filename: '[name].[contenthas].js'
-        }
+        filename: 'index.[contenthas].js'
+        },
+        plugins: [new HtmlWebpackPlugin({
+            title: 'XDML - 写代码啦',
+            template: 'src/assets/index.html'
+        })],
+        module: {
+            rules: [
+              {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+              },
+            ],
+          },
 };
+
